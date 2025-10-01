beb91f145bebebab888104ca920f9e1795f4cf68
Ardyan <ardyanrizki@icloud.com>
2025-10-01 21:18:05 +0700

    project init

---
+++ Stroll/ContentView.swift
@@ -0,0 +1,19 @@
+// © 2025 Ardyan - Pattern Matters. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stroll/StrollApp.swift
@@ -0,0 +1,12 @@
+// © 2025 Ardyan - Pattern Matters. All rights reserved.
+
+import SwiftUI
+
+@main
+struct StrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
